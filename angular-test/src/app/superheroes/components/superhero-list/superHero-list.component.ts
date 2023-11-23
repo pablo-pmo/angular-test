0bc783ee3d13922f2d71daed593ae959bb3f3c3c
pablo-pmo <impablopmo@gmail.com>
2023-11-23 16:04:49 +0100

    pagination added

--- angular-test/src/app/superheroes/components/superhero-list/superHero-list.component.ts
+++ angular-test/src/app/superheroes/components/superhero-list/superHero-list.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { SuperHero } from '../../interfaces/superHero';
-import { SuperHeroesService } from '../../services/superHeroes.service';
+
 import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
+
+import { SuperHeroesService } from '../../services/superHeroes.service';
+import { SuperHero } from '../../interfaces/superHero';
 import { DeletionDialogComponent } from '../deletion-dialog/deletion-dialog.component';
 
 @Component({
@@ -11,11 +14,28 @@ import { DeletionDialogComponent } from '../deletion-dialog/deletion-dialog.comp
 })
 export class SuperHeroListComponent {
 
-  public superHeroes: SuperHero[] = this.superHeroesService.getSuperHeroes();
-
   constructor(private superHeroesService: SuperHeroesService, private dialog: MatDialog) { }
 
+  public superHeroes: SuperHero[] = this.superHeroesService.getSuperHeroes();
+
+  public pagOptions = [3, 5, 10];
+
+  public pagStart: number = 0;
+
+  public pagItems: number = this.pagOptions[0];
+
   public currentSuperHero: SuperHero = this.restartCurrentSuperHero();
+
+  restartPagination(): void {
+    this.pagStart = 0;
+    this.pagItems = this.pagOptions[0];
+  }
+
+  changePagination(event: PageEvent): void {
+    console.log(event);
+    this.pagItems = event.pageSize;
+    this.pagStart = event.pageIndex;
+  }
 
   restartCurrentSuperHero(): SuperHero {
     return this.currentSuperHero = {
@@ -37,6 +57,7 @@ export class SuperHeroListComponent {
 
   onFilterHeroes(filterTerm: string): void {
     this.superHeroes = this.superHeroesService.getSuperHeroesByName(filterTerm);
+    this.restartPagination();
   }
 
   onDeleteSuperHero(heroClicked: SuperHero): void {
