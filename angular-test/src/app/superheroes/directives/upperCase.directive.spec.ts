dcc4ed2daa68dd48b4d55978417bf9ce9494ba6f
pablo-pmo <impablopmo@gmail.com>
2023-11-24 14:32:03 +0100

    deleted unused component tests

--- angular-test/src/app/superheroes/directives/upperCase.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UpperCaseDirective } from './upperCase.directive';
-
-describe('UpperCaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UpperCaseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
